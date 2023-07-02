62374a2e94cf3a8289f95466c5773204d805b8ab
valurathodp <valurathodp@gmail.com>
2023-07-02 08:10:13 +0530

    episode-03

--- app.js
+++ app.js
@@ -1,24 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
-);
-// console.log(heading);
+// JSX (transpiled before it reaches the JS) - Parcel - Babel
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-]);
-console.log(parent);
+// JSX => Babel transpiles it to React.createElement => React element - JS Object => HTMLElement(render)
+const jsxheading = (
+  <h1 className="heading" tabIndex="1">
+    Namaste React using JSX!
+  </h1>
+);
+
+// React Component
+// Class based component - Old way
+// Functional Component - New way
+
+// React Functional Component => is a function that returns some piece of JSX code => a function which returns react element
+
+// const HeadingComponent = () => {
+//   return <h1>Namaste React Functional Component</h1>;
+// };
+
+const Title = () => <h1>Namaste React Functional Component Title</h1>;
+
+// Component composition => Component inside component
+// const HeadingComponent = () => (
+//   <div id="container">
+//     <Title />
+//     <h1 className="heading">Namaste React Functional Component</h1>
+//   </div>
+// );
+
+// React Fragment - behaves like a empty tag (because JSX have only one root element - rather that using extra div tag we can use react.fragment)
+// const HeadingComponent = () => (
+//   <React.Fragment>
+//     <React.Fragment>
+//       <div id="container">
+//         <Title />
+//         <h1 className="heading">Namaste React Functional Component</h1>
+//       </div>
+//       <div id="container-2"></div>
+//     </React.Fragment>
+//   </React.Fragment>
+// );
+
+// React.Fragment looks ugely so react provides empty tag like - <></>
+const HeadingComponent = () => (
+  <>
+    <div id="container">
+      <Title />
+      <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+    <div id="container-2"></div>
+  </>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
